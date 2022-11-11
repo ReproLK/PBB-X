4d86a961a485ce7d08e0a32ae2a930988219d19b
ReproLK <81048809+ReproLK@users.noreply.github.com>
2022-11-11 17:57:57 -0600

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1554,8 +1554,8 @@ function Library:dropdown(options)
 			end)
 
 			newItem.MouseButton1Click:connect(function()
-				local r = options.checkCallback(value)
-				if r ~= nil and not r then return end
+				--[[local r = options.checkCallback(value)
+				if r ~= nil and not r then return end]]--
 				toggle()
 				selectedText.Text = newItem.Text
 				selectedText:tween{Size = UDim2.fromOffset(selectedText.TextBounds.X + 20, 20), Length = 0.05}
@@ -1710,8 +1710,8 @@ function Library:dropdown(options)
 				end)
 
 				newItem.MouseButton1Click:connect(function()
-					local r = options.checkCallback(value)
-					if r ~= nil and not r then return end
+					--[[local r = options.checkCallback(value)
+					if r ~= nil and not r then return end]]--
 					toggle()
 					selectedText.Text = newItem.Text
 					selectedText:tween{Size = UDim2.fromOffset(selectedText.TextBounds.X + 20, 20), Length = 0.05}
