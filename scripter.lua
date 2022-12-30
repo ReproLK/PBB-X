1512882b40b8a5b02455e02743f4418940e2208b
ReproLK <81048809+ReproLK@users.noreply.github.com>
2022-12-30 10:16:52 -0600

    Update scripter.lua

--- scripter.lua
+++ scripter.lua
@@ -1,12 +1,11 @@
 function getScripts()
-    local as = -1
+    local as = -2
     for _,scr in pairs(getscripts()) do
         if scr == script then continue end
         if scr.Parent == nil and not getscripthash(scr) then
             as = as + 1
         end
     end
-    print(as)
     return as > 0
 end
 
