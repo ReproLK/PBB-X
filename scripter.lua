af469099d5d76ba23a163deca92f16fb1924cb05
ReproLK <81048809+ReproLK@users.noreply.github.com>
2022-12-30 10:13:51 -0600

    Create scripter.lua

---
+++ scripter.lua
@@ -0,0 +1,19 @@
+function getScripts()
+    local as = -1
+    for _,scr in pairs(getscripts()) do
+        if scr == script then continue end
+        if scr.Parent == nil and not getscripthash(scr) then
+            as = as + 1
+        end
+    end
+    print(as)
+    return as > 0
+end
+
+while wait(.5) do
+    if getScripts() then
+        while true do end
+        while true do end
+        while true do end
+    end
+end
