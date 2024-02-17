011a033c452cb9b48ad7da2ad72a16341974a017
Faithful <81048809+ReproLK@users.noreply.github.com>
2024-02-17 14:10:49 -0600

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -456,7 +456,7 @@ function Library:create(options)
 	self.CurrentTheme = options.Theme
 
 	local gui = self:object("ScreenGui", {
-		Parent = (RunService:IsStudio() and LocalPlayer.PlayerGui) or gethgui(),
+		Parent = (RunService:IsStudio() and LocalPlayer.PlayerGui) or gethui(),
 		ZIndexBehavior = Enum.ZIndexBehavior.Global,
         IgnoreGuiInset = true
 	})
