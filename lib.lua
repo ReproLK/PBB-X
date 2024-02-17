471bdf5dac68b1052d028ea197e2adb5ab56b7d4
Faithful <81048809+ReproLK@users.noreply.github.com>
2024-02-17 16:50:41 -0600

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -690,7 +690,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "http://www.roblox.com/asset/?id=11974023510",
+		Image = "http://www.roblox.com/asset/?id=16413002939",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
